1bf013420c184ad7ce0bd60a01d9159ee7314142
Mark Steadman <marksteadman@me.com>
2014-05-25 15:39:10 +0100

    Updated Gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 command = '/opt/env/quickdraw.steadman.io/bin/gunicorn'
-pythonpath = '/opt/env/quickdraw.steadman.io/quickdraw.steadman.io/json'
+pythonpath = '/opt/env/quickdraw.steadman.io/quickdraw.steadman.io'
 bind = '127.0.0.1:8004'
 workers = 3
 user = 'quickdraw'
